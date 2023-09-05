1519a435c82ca024ef090d300c64a1f0a8303a5c
nasirabbas006 <138598219+nasirabbas006@users.noreply.github.com>
2023-09-05 11:16:27 +0500

    Update update_student_page.dart

--- lib/pages/update_student_page.dart
+++ lib/pages/update_student_page.dart
@@ -8,10 +8,120 @@ class UpdateStudentPage extends StatefulWidget {
 }
 
 class _UpdateStudentPageState extends State<UpdateStudentPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  updateUser() {
+    print('user update');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Update user page"),
-    );
+        appBar: AppBar(
+          title: Text("Update Student"),
+        ),
+        body: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      initialValue: 'nasir',
+                      autofocus: false,
+                      onChanged: (value) => {},
+                      decoration: InputDecoration(
+                        labelText: 'Name: ',
+                        labelStyle: TextStyle(fontSize: 20.0),
+                        border: OutlineInputBorder(),
+                        errorStyle:
+                            TextStyle(color: Colors.redAccent, fontSize: 15),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Name';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      initialValue: 'nasirtech601@gmai.com',
+                      autofocus: false,
+                      onChanged: (value) => {},
+                      decoration: InputDecoration(
+                        labelText: 'Email: ',
+                        labelStyle: TextStyle(fontSize: 20.0),
+                        border: OutlineInputBorder(),
+                        errorStyle:
+                            TextStyle(color: Colors.redAccent, fontSize: 15),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Email';
+                        } else if (!value.contains('@')) {
+                          return 'Please Enter Valid Email';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      initialValue: '123456',
+                      autofocus: false,
+                      onChanged: (value) => {},
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password: ',
+                        labelStyle: TextStyle(fontSize: 20.0),
+                        border: OutlineInputBorder(),
+                        errorStyle:
+                            TextStyle(color: Colors.redAccent, fontSize: 15),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Validate returns true if the form is valid, otherwise false.
+                            if (_formKey.currentState!.validate()) {
+                              Navigator.pop(context);
+                            }
+                          },
+                          child: Text(
+                            'Update',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () => {},
+                          child: Text(
+                            'Reset',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blueGrey),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )));
   }
 }
